6f0153c6b9a9c4d2edbb6f1a34241d515b38f92d
Alyssia4 <medeiros.anna@escolar.ifrn.edu.br>
2025-04-16 11:45:19 +0000

    atividade01

---
+++ atividade/ex2.py
@@ -0,0 +1,4 @@
+nota1 = int(input("Informe a primeira nota: "))
+nota2 = int(input("Informe a segunda nota: "))
+media_parcial = (nota1 * 2 + nota2 * 3) / 5
+print("A média parcial é:", media_parcial)
