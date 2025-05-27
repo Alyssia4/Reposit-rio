395b409edcb81c8784e7226a0bed658f5751929f
Alyssia4 <medeiros.anna@escolar.ifrn.edu.br>
2025-05-27 13:26:14 +0000

    lista

---
+++ listaPOO2/ex3.py
@@ -0,0 +1,29 @@
+class conta:
+    def __init__(self):
+        self.__titular = ""
+        self.__numero = ""
+        self.__saldo = "0"
+    def set_titular (self, t):
+        if t == "": raise ValueError()
+        self.__titular = t
+    def get_titular(self):
+        return self.__titular
+    def get_saldo(self):
+        return self.__saldo
+    def depositar(self, V):
+        if V < 0: raise ValueError()
+        self.__saldo += V
+    def sacar(self, V):
+        if V < 0 : raise ValueError()
+        if V > self.__saldo: raise ValueError()
+        self__salto -= V
+    class UI:
+        @staticmethod
+        def main():
+            x = conta()
+            x.set_titular(input())
+            x.set_numero(input())
+            x.depositar(float(input()))
+            print(f"Você tem {x.get_saldo()}")
+    UI.main()
+
